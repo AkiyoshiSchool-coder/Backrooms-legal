b7b632cc66b2afaa1b442f362f7c704b5bb8cb2c
Pedro <pedrogurskif@gmail.com>
2026-07-28 13:35:00 -0300

    fpc

---
+++ Backrooms-Balacobaco/Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstPersonLook : MonoBehaviour
+{
+    [SerializeField]
+    Transform character;
+    private float sensitivity = 2f;
+    public float baseSensivity = 2f;
+    public float smoothing = 1.5f;
+
+    Vector2 velocity;
+    Vector2 frameVelocity;
+
+    public InputActionAsset inputActions;
+    private InputAction lookAction;
+
+
+    void Reset()
+    {
+        // Get the character from the FirstPersonMovement in parents.
+        character = GetComponentInParent<FirstPersonMovement>().transform;
+    }
+
+    void Start()
+    {
+        // Lock the mouse cursor to the game screen.
+        Cursor.lockState = CursorLockMode.Locked;
+        lookAction = InputSystem.actions.FindAction("Look");
+
+        sensitivity = baseSensivity;
+    }
+
+    void Update()
+    {
+        // Get smooth velocity.
+        Vector2 mouseDelta = lookAction.ReadValue<Vector2>();
+        Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
+        frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
+        velocity += frameVelocity;
+        velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+
+        // Rotate camera up-down and controller left-right from velocity.
+        transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
+        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+    }
+
+    public void Freeze(bool freeze)
+    {
+        if(freeze)
+        {
+            sensitivity = 0;
+        }
+        else
+        {
+            sensitivity = baseSensivity;
+        }
+    }
+}
---
+++ Backrooms-Balacobaco/Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    [Header("Running")]
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
+    public float runSpeed = 9;
+    public InputActionAsset inputActions;
+    private InputAction runAction;
+    private InputAction moveAction;
+
+    Rigidbody rigidbody;
+    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        runAction = InputSystem.actions.FindAction("Run");
+    }
+
+    void FixedUpdate()
+    {
+        // Update IsRunning from input.
+        IsRunning = canRun && runAction.IsPressed();
+
+        // Get targetMovingSpeed.
+        float targetMovingSpeed = IsRunning ? runSpeed : speed;
+        if (speedOverrides.Count > 0)
+        {
+            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
+        }
+
+        // Get targetVelocity from input.
+        Vector2 targetVelocity = moveAction.ReadValue<Vector2>() * targetMovingSpeed;
+
+        // Apply movement.
+        rigidbody.linearVelocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.linearVelocity.y, targetVelocity.y);
+    }
+
+    public void Stop(bool stoporNot)
+    {
+        if(stoporNot)
+        {
+        speed = 0;
+        runSpeed = 0;
+        }
+        else
+        {
+            speed = 5;
+            runSpeed = 9;
+        }
+    }
+}
