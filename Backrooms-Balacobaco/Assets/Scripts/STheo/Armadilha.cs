8cec9f52fc80998e6d110795cb41ebdd5862ef31
theo <avalugggeludo@gmail.com>
2026-07-28 18:23:42 -0300

    67simga aura

---
+++ Backrooms-Balacobaco/Assets/Scripts/STheo/Armadilha.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Armadilha : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
+}
