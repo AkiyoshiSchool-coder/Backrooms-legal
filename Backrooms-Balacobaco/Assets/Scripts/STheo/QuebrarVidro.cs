007bcb3a3846dece131de9b685143eefc2e281f8
theo <avalugggeludo@gmail.com>
2026-08-04 14:56:38 -0300

    vidroarrumaod

--- Backrooms-Balacobaco/Assets/Scripts/STheo/QuebrarVidro.cs
+++ Backrooms-Balacobaco/Assets/Scripts/STheo/QuebrarVidro.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public class QuebrarVidro : MonoBehaviour
 {
+    [SerializeField] private float timer = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,8 +16,8 @@ public class QuebrarVidro : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision collision)
     {
-        Destroy(this.gameObject);
+        Destroy(this.gameObject, timer);
     }
 }
