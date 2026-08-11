c14f30ecef507aef2a0c70eab87f5f5040d8c3fd
Pedro <pedrogurskif@gmail.com>
2026-08-11 15:04:31 -0300

    menu

--- Backrooms-Balacobaco/Assets/Scripts/SHenrique/Menu.cs
+++ Backrooms-Balacobaco/Assets/Scripts/SHenrique/Menu.cs
@@ -4,9 +4,23 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject mainMenuObject, creditosObject, controlesObject;
+
     public void MudardeCena(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Creditos(bool open)
+    {
+        creditosObject.SetActive(open);
+        mainMenuObject.SetActive(!open);
+    }
+
+    public void Controles(bool open)
+    {
+        controlesObject.SetActive(open);
+        mainMenuObject.SetActive(!open);
     }
 
     public void Sair()
