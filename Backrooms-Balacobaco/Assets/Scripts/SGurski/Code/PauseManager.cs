205a102dce589446ff3ae2fac31e1f590c1e9db9
Pedro <pedrogurskif@gmail.com>
2026-08-25 16:55:21 -0300

    commit

--- Backrooms-Balacobaco/Assets/Scripts/SGurski/Code/PauseManager.cs
+++ Backrooms-Balacobaco/Assets/Scripts/SGurski/Code/PauseManager.cs
@@ -6,28 +6,38 @@ public class PauseManager : MonoBehaviour
 {
     public GameObject controlesMenu, pauseMenu;
     public PlayerInteraction playerCode;
+    public StudioEventEmitter audioEmitter;
 
     public void Controles()
     {
+        ClickButton();
         pauseMenu.SetActive(false);
         controlesMenu.SetActive(true);
     }
 
     public void Voltar()
     {
+        ClickButton();
         pauseMenu.SetActive(true);
         controlesMenu.SetActive(false);
     }
 
     public void Continuar()
     {
+        ClickButton();
         playerCode.Pausar();
     }
 
     public void Menu()
     {
+        ClickButton();
         Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene("MenuInicial");
+    }
+    
+    public void ClickButton()
+    {
+        audioEmitter.Play();
     }
 }
