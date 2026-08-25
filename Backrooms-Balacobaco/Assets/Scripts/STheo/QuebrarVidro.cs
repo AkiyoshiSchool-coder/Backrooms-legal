041ac0f30c3eaaa08f4c9d7c59469a206de4e186
theo <avalugggeludo@gmail.com>
2026-08-25 17:15:24 -0300

    caco

--- Backrooms-Balacobaco/Assets/Scripts/STheo/QuebrarVidro.cs
+++ Backrooms-Balacobaco/Assets/Scripts/STheo/QuebrarVidro.cs
@@ -6,11 +6,13 @@ public class QuebrarVidro : MonoBehaviour
 {
     [SerializeField] private float timer = 0f;
     [SerializeField] private StudioEventEmitter glassBreak;
+    [SerializeField] GameObject cacoVidro;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void OnCollisionEnter(Collision collision)
     {
         glassBreak.Play();
+        Instantiate(cacoVidro, transform.position, Quaternion.identity);
         Destroy(this.gameObject, timer);
     }
 }
