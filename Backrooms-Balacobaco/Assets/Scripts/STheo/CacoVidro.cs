6b32ab1d0dc65b48553a8ebbc50820d17420d2b3
theo <avalugggeludo@gmail.com>
2026-09-01 14:52:30 -0300

    carla gostou do caco de vidro

--- Backrooms-Balacobaco/Assets/Scripts/STheo/CacoVidro.cs
+++ Backrooms-Balacobaco/Assets/Scripts/STheo/CacoVidro.cs
@@ -1,24 +1,31 @@
 using UnityEngine;
-
+using System.Collections;
+using Unity.VisualScripting;
 public class CacoVidro : MonoBehaviour
 {
     private float speed = 7f;
     bool parar = false;
+    [SerializeField] private float timer = 1.77f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //StartCoroutine("Timer");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        //transform.Translate(Vector3.down * speed * Time.deltaTime);
     }
-
-    void OnCollisionEnter(Collision other)
+    IEnumerator Timer()
     {
-        print("coco");
+        yield return new WaitForSeconds(timer);
         Destroy(this.gameObject);
+        yield break;
     }
+   // void OnTriggerEnter(Collider other)
+   // {
+  //      print("coco");
+ //       Destroy(this.gameObject);
+   /// }
 }
